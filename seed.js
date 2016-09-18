7fe58a65bfb2e6c01a6e276b7c52a84fdc0203b8
Roger Cores <rogercores@gmail.com>
2016-09-18 20:23:45 +0530

    Update

--- seed.js
+++ seed.js
@@ -8,6 +8,47 @@ module.exports = function(models, ObjectId){
       console.log(err);
   }
 
+
+  var createRider = function(name, email, password, vehicleNumber, phoneNumber){
+    var newIdx = new models.ID();
+    newIdx._id = new ObjectId();
+    newIdx.email = email;
+    newIdx.name = name;
+    newIdx.phone = phoneNumber;
+    newIdx.userType = "RIDER";
+    newIdx.password = newIdx.generateHash(password);
+    newIdx.save(callback);
+
+    var riderx = {
+      _id: new ObjectId(),
+      user: newIdx._id,
+      vehicleNumber: vehicleNumber
+    }
+
+    new models.Rider(riderx).save(callback);
+  }
+
+  var createBaker = function(name, email, password, phoneNumber, address, referal, locality){
+    var newId = new models.ID();
+    newId._id = new ObjectId();
+    newId.email = email;
+    newId.name = name;
+    newId.phone = phoneNumber;
+    newId.userType = "BAKER";
+    newId.password = newId.generateHash(password);
+    newId.save(callback);
+
+    var baker = {
+      _id: new ObjectId(),
+      user: newId._id,
+      locality: locality,
+      address: address,
+      referal: referal
+    }
+
+    new models.Baker(baker).save(callback);
+  }
+
   //remove existing data
   var keys = Object.keys(models);
   for(var key in keys){
@@ -57,68 +98,85 @@ module.exports = function(models, ObjectId){
       value: 150
     }
 
-    
+
 
     var localities = [{"name": "IC Colony, Borivali", _id: new ObjectId()},{"name": "Anand Nagar, Thane"},{"name": "Kandarpada, Dahisar"}];
 
 
     //Create a Baker
-    var newId = new models.ID();
-    newId._id = new ObjectId();
-    newId.email = "soulsugar";
-    newId.name = "rogercores";
-    newId.phone = 8655814592;
-    newId.userType = "BAKER";
-    newId.password = newId.generateHash("soulsugar");
-    newId.save(callback);
 
-    var baker = {
-      _id: new ObjectId(),
-      user: newId._id,
-      locality: localities[0]._id,
-      address: "yalagaadh",
-      referal: "XLS345"
-    }
-
-    new models.Baker(baker).save(callback);
+    createBaker("soulsugar", "soulsugar", "password", "5698785456", "Kandarpada, Dahisar (w)", "CAK536", localities[0]._id);
+    createBaker("cakestar", "cakestar", "password", "9865897456", "Kandarpada, Dahisar (w)", "CAK556", localities[0]._id);
+    createBaker("gluttony", "gluttony", "password", "8975463259", "Kandarpada, Dahisar (w)", "CAK336", localities[0]._id);
+    createBaker("patisseriehome", "thephome", "password", "9236598745", "Kandarpada, Dahisar (w)", "CAK236", localities[0]._id);
+    createBaker("r9designer", "r9designer", "password", "7986594213", "Kandarpada, Dahisar (w)", "CAK531", localities[0]._id);
+    createBaker("theobroma", "theobroma", "password", "7896512365", "Powai, Andheri (w)", "CAK530", localities[0]._id);
 
 
-    //Create a RIDER
-    var newId = new models.ID();
-    newId._id = new ObjectId();
-    newId.email = "cakestar";
-    newId.name = "Maity";
-    newId.phone = 7506908493;
-    newId.userType = "RIDER";
-    newId.password = newId.generateHash("cakestar");
-    newId.save(callback);
+    createRider("Nikhil", "nikhil", "password", "MH 456895", "8965874596");
+    createRider("Roger", "roger", "password", "MH 986598", "5897462135");
+    createRider("Maitreya", "matty", "password", "MH 002564", "5546956221");
+    createRider("Minal", "minal", "password", "MH 986545", "6054987562");
+    createRider("Atmaram", "atmaram", "password", "MH 320159", "4589875624");
+    createRider("Ardhchandra", "ardhchandra", "password", "MH 987654", "9876521368");
 
-    var rider = {
-      _id: new ObjectId(),
-      user: newId._id,
-      vehicleNumber: "MH AIBSNGEIC45"
-    }
-
-    new models.Rider(rider).save(callback);
-
-    var newIdx = new models.ID();
-    newIdx._id = new ObjectId();
-    newIdx.email = "rider";
-    newIdx.name = "Maityx";
-    newIdx.phone = 0000000000;
-    newIdx.userType = "RIDER";
-    newIdx.password = newIdx.generateHash("rider");
-    newIdx.save(callback);
-
-    var riderx = {
-      _id: new ObjectId(),
-      user: newIdx._id,
-      vehicleNumber: "MH ASDFEBDG3"
-    }
-
-    new models.Rider(riderx).save(callback);
-
-
+    // var newId = new models.ID();
+    // newId._id = new ObjectId();
+    // newId.email = "soulsugar";
+    // newId.name = "rogercores";
+    // newId.phone = 8655814592;
+    // newId.userType = "BAKER";
+    // newId.password = newId.generateHash("soulsugar");
+    // newId.save(callback);
+    //
+    // var baker = {
+    //   _id: new ObjectId(),
+    //   user: newId._id,
+    //   locality: localities[0]._id,
+    //   address: "yalagaadh",
+    //   referal: "XLS345"
+    // }
+    //
+    // new models.Baker(baker).save(callback);
+    //
+    //
+    // var newId = new models.ID();
+    // newId._id = new ObjectId();
+    // newId.email = "cakestar";
+    // newId.name = "matty";
+    // newId.phone = 7506908493;
+    // newId.userType = "BAKER";
+    // newId.password = newId.generateHash("cakestar");
+    // newId.save(callback);
+    //
+    // var cakestarBaker = {
+    //   _id: new ObjectId(),
+    //   user: newId._id,
+    //   locality: localities[0]._id,
+    //   address: "yalagaadh",
+    //   referal: "XLS369"
+    // }
+    //
+    // new models.Baker(cakestarBaker).save(callback);
+    //
+    // var newIdx = new models.ID();
+    // newIdx._id = new ObjectId();
+    // newIdx.email = "rider";
+    // newIdx.name = "Maityx";
+    // newIdx.phone = 0000000000;
+    // newIdx.userType = "RIDER";
+    // newIdx.password = newIdx.generateHash("rider");
+    // newIdx.save(callback);
+    //
+    // var riderx = {
+    //   _id: new ObjectId(),
+    //   user: newIdx._id,
+    //   vehicleNumber: "MH ASDFEBDG3"
+    // }
+    //
+    // new models.Rider(riderx).save(callback);
+    //
+    //
 
 
     // var customer = {
