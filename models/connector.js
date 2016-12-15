a3acb7e2503f5c54a97ad5a43a798f2c464a9ee3
roger-cores <rogercores2@gmail.com>
2016-12-15 11:04:55 +0530

    Update

--- models/connector.js
+++ models/connector.js
@@ -1,7 +1,7 @@
 module.exports = function(mongoose, env){
   var ur = "";
-  // url = ur.concat("mongodb://",env.username,":",env.password,"@",env.host,":",env.port,"/",env.database);
-  url = ur.concat("mongodb://",env.host,":",env.port,"/",env.database);
+  url = ur.concat("mongodb://",env.username,":",env.password,"@",env.host,":",env.port,"/",env.database);
+  //url = ur.concat("mongodb://",env.host,":",env.port,"/",env.database);
 
   console.log(url);
   mongoose.connect(url);
